8de997abe42b72dafb5f346a3f8cd2d1829fdea3
valtheval <valentin.masdeu@gmail.com>
2022-02-16 08:54:50 +0100

    drop lines outside boundaries for latitude and longitude features

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -30,6 +30,8 @@ def go(args):
         idx = df["price"].between(args.min_price, args.max_price)
         df = df[idx].copy()
         df["last_review"] = pd.to_datetime(df["last_review"])
+        idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+        df = df[idx].copy()
 
         df.to_csv("clean_sample.csv", index=False)
         artifact = wandb.Artifact(args.output_artifact, type=args.output_type, description=args.output_description
